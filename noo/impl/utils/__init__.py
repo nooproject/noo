e2c7bdd3d3047e2c8b7c9b1d14e71f2fa980176c
vcokltfre <vcokltfre@gmail.com>
2022-03-10 16:18:24 +0000

    chore: clean up old files after rewriting registry

--- noo/impl/utils/__init__.py
+++ noo/impl/utils/__init__.py
@@ -1,12 +1,10 @@
 from .echo import echo, set_quiet
 from .errors import NooException, cancel
-from .resolver import Resolver
 from .store import STORE, FileStore, Store
 
 __all__ = (
     "FileStore",
     "NooException",
-    "Resolver",
     "Store",
     "STORE",
     "cancel",
