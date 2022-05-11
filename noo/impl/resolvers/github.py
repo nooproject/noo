724cd96d8e57401077d3099373b10b1aada25eda
vcokltfre <vcokltfre@gmail.com>
2022-05-11 22:26:37 +0100

    chore: remove git folder after clone git

--- noo/impl/resolvers/github.py
+++ noo/impl/resolvers/github.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
 from pathlib import Path
+from shutil import rmtree
 
 from git.repo import Repo
 
 
 def clone_github(repo: str, dest: Path) -> None:
     Repo.clone_from(repo, dest)  # type: ignore
+
+    rmtree(dest / ".git")
